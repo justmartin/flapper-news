f22f951062cabad4f03dcf741dd213d53bcdb820
justmartin <justin.lamar.martin@gmail.com>
2017-01-16 11:01:40 -0800

    Removed extra root route assignment.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root to: 'application#angular'
 
-  root to: 'application#angular'
-
   resources :posts, only: [:create, :index, :show] do
-    resources :comments, only: [:show, :create] do
+    resources :comments, only: [:show, :create] do 
       member do
         put '/upvote' => 'comments#upvote'
       end
