9adc76a1329399e356afafa8185382d471825698
justmartin <justin.lamar.martin@gmail.com>
2017-01-17 12:17:58 -0800

    Formatted spacing in controllers.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,14 +14,13 @@ class PostsController < ApplicationController
 
   def upvote
     post = Post.find(params[:id])
-
     post.increment!(:upvotes)
 
     respond_with post
   end
 
   private
-
+  
   def post_params
     params.require(:post).permit(:link, :title)
   end
