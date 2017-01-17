c79d96b2e6773f7ff1f5aa37a693d72ee2842ada
justmartin <justin.lamar.martin@gmail.com>
2017-01-17 10:28:17 -0800

    Added getAll() to get all posts in the service within posts.js.

--- app/assets/javascripts/posts/post.js
+++ app/assets/javascripts/posts/post.js
@@ -1,6 +1,10 @@
-angular.module('flapperNews').factory('posts', [function(){
+angular.module('flapperNews').factory('posts', ['$http', function($http) {
 	var o = {
 		posts: []
 	};
 	return o;
+
+	o.getAll $http.get('/posts.json').success(function(data) {
+		angular.copy(data, o.posts);
+	});
 }]);
